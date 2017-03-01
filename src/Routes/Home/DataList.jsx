180bc621617bc96073c2c3f68b7e4dac6d9a387b
Fadly Kayo <fadly.kayo@googlemail.com>
2017-03-02 00:27:45 +0700

    Add fetch news

--- src/Routes/Home/DataList.jsx
+++ src/Routes/Home/DataList.jsx
@@ -1,24 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { setNews } from '../../actions'
 
-const DataList = (props) => {
-  return (
-    <ul>
-      {props.news
-         .filter(eachNews => {
-           return (eachNews.title === null ? '' : eachNews.title).match(new RegExp(props.searchKey, 'i'))
-         })
-         .map((item, index) => {
-           return (
-             <li key={index}>
-               <a href={item.url}>
-                 {item.title}
-               </a>
-             </li>
-           )
-         })}
-    </ul>
-  )
+class DataList extends Component {
+  componentDidMount () {
+    this.fetchNews('')
+  }
+
+  fetchNews (searchQuery) {
+    fetch(`https://hn.algolia.com/api/v1/search?query=${encodeURI(searchQuery)}`)
+      .then((response) => {
+        return response.json()
+      })
+      .then((resp) => {
+        this.setNews(resp.hits)
+      })
+  }
+
+  render () {
+    return (
+      <ul>
+        {this.props.news
+           .filter(eachNews => {
+             return (eachNews.title === null ? '' : eachNews.title).match(new RegExp(this.props.searchKey, 'i'))
+           })
+           .map((item, index) => {
+             return (
+               <li key={index}>
+                 <a href={item.url}>
+                   {item.title}
+                 </a>
+               </li>
+             )
+           })}
+      </ul>
+    )
+  }
+
 }
 
 const mapStateToProps = (state) => {
@@ -28,4 +47,8 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect(mapStateToProps)(DataList)
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({setNews}, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(DataList)
