65b84a55e8bf00a83ff886988e3c481a8eee439b
Syanmil <syanmil@yahoo.com>
2017-03-01 12:55:00 +0700

    create store

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+import News from './Routes/News'
+import People from './Routes/People'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+const App = () => (
+  <Router>
+    <div className='App'>
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to Syanmil React</h2>
       </div>
-    );
-  }
-}
+      <ul>
+        <li><Link to="/">News</Link></li>
+        <li><Link to="/people">People</Link></li>
+      </ul>
 
-export default App;
+      <hr/>
+
+      <Route exact path="/" component={News}/>
+      <Route path="/people" component={People}/>
+    </div>
+  </Router>
+)
+
+export default App
